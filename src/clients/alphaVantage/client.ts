0b8b8f0f96c0f0585512d2b69c54785c8a877357
josh-W42 <wilson.jd022@gmail.com>
2022-07-15 13:38:28 -0700

    Using sleep helper function to retry calling the API when we've hit the limit

--- src/clients/alphaVantage/client.ts
+++ src/clients/alphaVantage/client.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosError } from 'axios';
+import { sleep } from '../../helpers';
 import { BaseQuery } from './queries';
 
 interface IRequestConfig {
@@ -19,26 +20,37 @@ export class AlphaVantageClient {
   }: IRequestConfig): Promise<T | undefined> {
     const url = query.URL;
 
-    try {
-      const response = await axios(url);
-      if (response.data['Error Message']) {
-        throw new AxiosError(response.data['Error Message'], '400');
-      }
+    let receivedData = false;
+    while (!receivedData) {
+      try {
+        console.log('Attempting to access AV API...');
+        const response = await axios(url);
 
-      if (response.data['Note']) {
-        throw new AxiosError(
-          `Too Many Requests: ${response.data['Note']}`,
-          '429'
+        if (response.data['Error Message']) {
+          // There was an error.
+          throw new AxiosError(response.data['Error Message'], '400');
+        }
+
+        if (response.data['Note']) {
+          // Too many Requests
+          console.log(
+            `Warning: API Request Limit Reached: ${query.params?.function}: [${response.data['Note']}]`
+          );
+          await sleep(60_000);
+          continue;
+        }
+
+        // We've received the data we need.
+        receivedData = true;
+        console.log(
+          `GET: ${query.params?.function} [${response.statusText} : ${response.status}]`
         );
-      }
 
-      console.log(
-        `GET: ${query.params?.function} [${response.statusText} : ${response.status}]`
-      );
-      return response.data;
-    } catch (error) {
-      console.log(`GET ERROR: ${query.params?.function}: [${error}]`);
-      return undefined;
+        return response.data;
+      } catch (error) {
+        console.log(`GET ERROR: ${query.params?.function}: [${error}]`);
+        return undefined;
+      }
     }
   }
 }
